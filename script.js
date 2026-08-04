fe90a3f4a72dcc14a3f458b141147873af1fe08a
Bhagyashri sharma <bhagyashreesharama@gmail.com>
2026-08-04 13:16:05 +0530

    Implement love letter typing effect
    Added a love letter typing effect that displays a message when a button is clicked.

--- script.js
+++ script.js
@@ -63,3 +63,70 @@ gift.style.transform="";
 
 
 });
+// LOVE LETTER TYPE EFFECT
+
+
+const letterButton =
+document.getElementById("openLetter");
+
+
+const letterBox =
+document.getElementById("letterBox");
+
+
+const letterText =
+document.getElementById("letterText");
+
+
+
+const message =
+
+"You came into my life like a beautiful surprise. \
+Thank you for every smile, every conversation and every moment. \
+You are someone who makes ordinary days feel special. \
+I hope this little world always reminds you how much you mean to me. 💖";
+
+
+
+if(letterButton){
+
+
+letterButton.addEventListener("click",()=>{
+
+
+letterBox.style.display="block";
+
+
+let index=0;
+
+
+letterText.innerHTML="";
+
+
+
+function typing(){
+
+
+if(index < message.length){
+
+
+letterText.innerHTML += message[index];
+
+index++;
+
+setTimeout(typing,60);
+
+
+}
+
+
+}
+
+
+
+typing();
+
+
+});
+
+}
